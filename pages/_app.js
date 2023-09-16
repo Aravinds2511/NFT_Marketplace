79800e48b900f055afd737dfc832749e4c1e375e
arvndblocker"nn <aravindvasang@gmail.com>
2023-09-16 19:41:12 +0530

    nft_marketplace

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="test-4xl font-bold">---NFT Market---</p>
+        <p className="test-10xl font-bold">Nftverse Market</p>
         <div className="flex mt-4">
-          <Link href="/">
+          <Link href="/" legacyBehavior>
             <a className="mr-4 text-pink-500">Home</a>
           </Link>
-          <Link href="/create-nft">
+          <Link href="/create-nft" legacyBehavior>
             <a className="mr-6 text-pink-500">Sell NFT</a>
           </Link>
-          <Link href="/my-nfts">
+          <Link href="/my-nfts" legacyBehavior>
             <a className="mr-6 text-pink-500">MY NFTs</a>
           </Link>
-          <Link href="/dashboard">
+          <Link href="/dashboard" legacyBehavior>
             <a className="mr-6 text-pink-500">Dashboard</a>
           </Link>
         </div>
